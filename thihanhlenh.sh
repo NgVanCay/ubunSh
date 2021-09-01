5edd29785432caeb3c4fa6858319bc4c99487269
NgVanCay <89650170+NgVanCay@users.noreply.github.com>
2021-09-01 14:00:05 +0700

    Create thihanhlenh.sh

---
+++ thihanhlenh.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz && tar -zxf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u XMR:84dhzEzJVsUY3h3L8QCy1oVBpEQowbeGU7CRmmWyB7KFUnD1iitd7Pdd9n6MYEJSS9cfXmgPkGsK4DsXJzuWZhPQHTY7Snd.Csb07#ysc8-2gp5 -p x
